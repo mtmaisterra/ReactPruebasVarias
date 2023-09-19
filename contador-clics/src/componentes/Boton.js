0931a0d76826071853208dc27efd60f16323e0b9
Teresa Maisterra <mtmaisterra@gmail.com>
2023-09-19 12:48:13 -0300

    commit 4

--- contador-clics/src/componentes/Boton.js
+++ contador-clics/src/componentes/Boton.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../hojas-de-estilo/Boton.css'
 
-function Boton({texto, esBotonDeClic}){
-return
+function Boton({texto, esBotonDeClic, manejarClic}){
+return(
 <button 
-className={esBotonDeClic ? 'boton-clic' : 'boton-reiniciar'}>    
+className={esBotonDeClic ? 'boton-clic' : 'boton-reiniciar'}
+onClick={manejarClic}>    
 {texto}
 </button>
+);
 }
+
+export default Boton;
