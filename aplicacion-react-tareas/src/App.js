cc8e26132927d7ec30e3f7dcf3289cdc7347a3c8
Teresa Maisterra <mtmaisterra@gmail.com>
2023-09-20 19:27:57 -0300

    commit 9

--- aplicacion-react-tareas/src/App.js
+++ aplicacion-react-tareas/src/App.js
@@ -1,12 +1,18 @@
-
 import './App.css';
+import freeCodeCampLogo from './imagenes/Logo-freeCodeCamp.png'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='aplicacion-tareas'>
+     <div className='freecodecamp-logo-contenedor'>
+<img 
+src={freeCodeCampLogo} 
+  className='freecodecamp-logo' />
+     </div>
     </div>
   );
 }
 
 export default App;
+
+/*se recomienda dejar un espacio antes /> */
